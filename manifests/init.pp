891996e5c80dc06ed90019b8d45e3ba013d0377c
Spencer Seebald <sseebald@gmail.com>
2014-09-12 15:41:08 +0000

    Initial commit - installs dotnet 4.0 on server2k8r2

---
+++ manifests/init.pp
@@ -0,0 +1,28 @@
+class dotnet {
+
+  require profile::staging
+
+  if $kernelmajversion == '6.1' {
+    reboot { 'before dotnet install':
+      when => pending,
+    }
+
+    file { 'dotNetFx40_Full_x86_x64.exe':
+      path   => 'C:\staging\dotNetFx40_Full_x86_x64.exe',
+      mode   => 0755,
+      source => 'puppet:///modules/cmsapp/dotNetFx40_Full_x86_x64.exe',
+      before => Package['Microsoft .NET Framework 4 Client Profile'],
+    }
+
+    package { 'Microsoft .NET Framework 4 Client Profile':
+      ensure          => installed,
+      source          => 'C:\staging\dotNetFx40_Full_x86_x64.exe',
+      install_options => ['/q', '/norestart'],
+    }
+
+    reboot { 'successful dotnet install':
+      subscribe => Package['Microsoft .NET Framework 4 Client Profile'],
+    }
+  }
+
+}
