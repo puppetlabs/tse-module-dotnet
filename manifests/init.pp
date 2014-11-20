77c4fe9c99da741ae9c8fda90f47b4ea3c6f99ec
Chris Barker <cbarker@puppetlabs.com>
2014-11-20 11:59:24 -0800

    fix for working in 3.7 final

--- manifests/init.pp
+++ manifests/init.pp
@@ -7,16 +7,14 @@ class dotnet {
       when => pending,
     }
 
-    file { 'dotNetFx40_Full_x86_x64.exe':
-      path   => 'C:\staging\dotNetFx40_Full_x86_x64.exe',
-      mode   => 0755,
+    staging::file { 'dotNetFx40_Full_x86_x64.exe':
       source => "http://${::servername}/dotnetcms/dotNetFx40_Full_x86_x64.exe",
       before => Package['Microsoft .NET Framework 4 Client Profile'],
     }
 
     package { 'Microsoft .NET Framework 4 Client Profile':
       ensure          => installed,
-      source          => 'C:\staging\dotNetFx40_Full_x86_x64.exe',
+      source          => 'C:\staging\dotnet\dotNetFx40_Full_x86_x64.exe',
       install_options => ['/q', '/norestart'],
     }
 
